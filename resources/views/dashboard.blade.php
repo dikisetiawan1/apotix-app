5bbf8a7b47d62f61b73ce8ec8600b2b5244cf0f6
dikisetiawan1 <dikisetia101@gmail.comgit config --global user.email dikisetia101@gmail.comgit config --global user.email dikisetia101@gmail.com>
2023-06-06 23:02:29 +0700

    first upload

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Apotix-app</title>
+</head>
+<body>
+
+    <h1>Keep Learning With Lara!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni dolorum vel nemo facilis molestias, deserunt officia, impedit animi sit magnam ab voluptatibus. Eius ex laudantium, quisquam explicabo quod alias! Ut.</p>
+    
+</body>
+</html>
